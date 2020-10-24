e5b2e5cd63dfb4bb4fd758faf317e796ea73ef75
Abhishek <abhishektupudana@gmail.com>
2020-10-24 17:14:30 +0530

    form handler

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,16 +1,22 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-    $email_from = "abhishekrnc790@gmail.com";
+$email_from = "abhishekrnc790@gmail.com";
 
-    $email_subject = "New Enquiry";
+$email_subject = "New Enquiry";
 
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "User Message: $message.\n";
 
-    $to = "abhishektupudana@gmail.com"
+$to = "abhishektupudana@gmail.com";
 
-    
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html");
