6fad9056461af7248fd3e97124d7c25770a9224f
Abhishek <abhishektupudana@gmail.com>
2020-10-24 17:11:20 +0530

    php form handler

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,3 +2,15 @@
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
+
+    $email_from = "abhishekrnc790@gmail.com";
+
+    $email_subject = "New Enquiry";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "abhishektupudana@gmail.com"
+
+    
